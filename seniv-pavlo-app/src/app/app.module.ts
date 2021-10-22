a0650bd9571baacff553e7ecf591c840db12b4c9
Seniv Pavlo <senivpavlo016@gmail.com>
2021-10-22 16:22:29 +0300

    Add class mat-typography. And create in catalog.com.ts items[]. And create template for catalog.component.html. Add style in style.scss

--- seniv-pavlo-app/src/app/app.module.ts
+++ seniv-pavlo-app/src/app/app.module.ts
@@ -3,20 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CatalogComponent } from './catalog/catalog.component';
-import { MatToolbarModule} from '@angular/material/toolbar'
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { MatListModule } from '@angular/material/list';
 @NgModule({
-  declarations: [
-    AppComponent,
-    CatalogComponent
-  ],
-  imports: [
-    BrowserModule,
-    MatToolbarModule,
-    FlexLayoutModule
-  ],
+  declarations: [AppComponent, CatalogComponent],
+  imports: [BrowserModule, MatToolbarModule, FlexLayoutModule, MatListModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- seniv-pavlo-app/src/app/catalog/catalog.component.ts
+++ seniv-pavlo-app/src/app/catalog/catalog.component.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Item } from '../shared/item';
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
-  styleUrls: ['./catalog.component.scss']
+  styleUrls: ['./catalog.component.scss'],
 })
 export class CatalogComponent implements OnInit {
+  items: Item[] = [
+    {
+      id: '0',
+      name: 'Item A-1',
+      image: '../assets/images/A/A-1.png',
+      category: 'A',
+      featured: true,
+      label: 'L-1',
+      price: '4.99',
+      description: 'Lorem20',
+    },
+    {
+      id: '0',
+      name: 'Item A-1',
+      image: '../assets/images/A/A-1.png',
+      category: 'A',
+      featured: true,
+      label: 'L-1',
+      price: '4.99',
+      description: 'Lorem20',
+    },
+    {
+      id: '0',
+      name: 'Item A-1',
+      image: '../assets/images/A/A-1.png',
+      category: 'A',
+      featured: true,
+      label: 'L-1',
+      price: '4.99',
+      description: 'Lorem20',
+    },
+    {
+      id: '0',
+      name: 'Item A-1',
+      image: '../assets/images/A/A-1.png',
+      category: 'A',
+      featured: true,
+      label: 'L-1',
+      price: '4.99',
+      description: 'Lorem20',
+    },
+    {
+      id: '0',
+      name: 'Item A-1',
+      image: '../assets/images/A/A-1.png',
+      category: 'A',
+      featured: true,
+      label: 'L-1',
+      price: '4.99',
+      description: 'Lorem20',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
