680e6960d8aa58668756893c7531f97f3d47ada4
Seniv Pavlo <senivpavlo016@gmail.com>
2021-10-22 17:44:31 +0300

    In app.module add new Module. catalog.coponents create template for item. In .ts(this comp) create const array object

--- seniv-pavlo-app/src/app/app.module.ts
+++ seniv-pavlo-app/src/app/app.module.ts
@@ -6,9 +6,20 @@ import { CatalogComponent } from './catalog/catalog.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [AppComponent, CatalogComponent],
-  imports: [BrowserModule, MatToolbarModule, FlexLayoutModule, MatListModule],
+  imports: [
+    BrowserModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
